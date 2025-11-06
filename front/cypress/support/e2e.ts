066b4a1cb6c4f690b4cb88c53f5805ab298ecb94
Chrys <deregnaucourt.chrystopher@hotmail.fr>
2025-11-06 22:11:28 +0100

    Tests E2E workflow

--- front/cypress/support/e2e.ts
+++ front/cypress/support/e2e.ts
@@ -12,8 +12,4 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
-// When a command from ./commands is ready to use, import with `import './commands'` syntax
-import './commands';
-
 import '@cypress/code-coverage/support';
